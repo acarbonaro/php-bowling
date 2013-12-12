4bc493e818ce91dabfc859ca4ce427b04c56d9c9
Anthony Carbonaro <anthony@anthonycarbonaro.com>
2013-12-12 08:32:37 -0800

    Added spare and unit tests

--- BowlingGame.php
+++ BowlingGame.php
@@ -2,15 +2,32 @@
 
 class BowlingGame
 {
-    protected $score = 0;
+    protected $rolls = array();
 
     public function roll($pins)
     {
-        $this->score += $pins;
+        $this->score  += $pins;
+        $this->rolls[] = $pins;
     }
 
     public function score()
     {
-        return $this->score;
+        $score      = 0;
+        $frameIndex = 0;
+
+        for ($frame = 0; $frame < 10; $frame++) {
+            if ($this->isSpare($frameIndex)) {
+                $score += 10 + $this->rolls[$frameIndex + 2];
+            } else {
+                $score += $this->rolls[$frameIndex] + $this->rolls[$frameIndex + 1];
+            }
+            $frameIndex += 2;
+        }
+
+        return $score;
+    }
+
+    protected function isSpare($frameIndex) {
+        return $this->rolls[$frameIndex] + $this->rolls[$frameIndex + 1] == 10;
     }
 }
--- BowlingGameTest.php
+++ BowlingGameTest.php
@@ -29,4 +29,18 @@ class BowlingGameTest extends PHPUnit_Framework_TestCase
         $this->rollMany(20, 1);
         $this->assertEquals(20, $this->game->score());
     }
+
+    public function testOneSpare()
+    {
+        $this->rollSpare();
+        $this->game->roll(3);
+        $this->rollMany(17, 0);
+        $this->assertEquals(16, $this->game->score());
+    }
+
+    public function rollSpare()
+    {
+        $this->game->roll(5);
+        $this->game->roll(5);
+    }
 }
